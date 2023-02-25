8fe39756f3929800d60e80774db1ae0f7081983f
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-02-24 22:14:01 -0800

    add proxy

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,3 +1,8 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
